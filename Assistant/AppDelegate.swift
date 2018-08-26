e6004a5da5fdc73d02c28288b29596b79f0b209e
mathieu <mathieu@dynamic.wline.6rd.res.cust.swisscom.ch>
2018-08-26 04:05:14 +0200

    Initial Commit

---
+++ Assistant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Assistant
+//
+//  Created by mathieu on 26.08.18.
+//  Copyright © 2018 Mathieu Knecht. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
